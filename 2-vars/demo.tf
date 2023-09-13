7c34b970ee417f6b2c916098e6c800ae6a9bd8c4
Lokesh Karasala <lokeshkarasala@Lokeshs-iMac.local>
2023-09-13 21:36:52 +0530

    output

--- 2-vars/demo.tf
+++ 2-vars/demo.tf
@@ -1,8 +1,30 @@
+
+# Declaring a variable
+
 variable "sample" {
-    default = "Hellow world"
+    default = "Hello world"
 }
 
+# Printing an output
+'''''
 output "op" {
     value = var.sample
 }
+''''
 
+# whenever you want to print a variable in the group of words, you need to use bash syntax
+
+output "ops" {
+    value = My first message is ${var.sample}"
+}
+
+# A variable can be directly accessed by calling it as var.variableName, but if you're accessing in between strings need to enclose them.
+# In terraform there is no concept of the single quotes.
+
+variable "number" {
+    default = 100
+}
+
+output "op_number" {
+    value = var.number
+}
